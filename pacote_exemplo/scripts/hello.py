ada850b83a4e24494bb4472a6a19f6e3db9079c4
felipebpl <felipebpl@al.insper.edu.br>
2022-09-22 10:29:50 -0300

    adding translation

--- pacote_exemplo/scripts/hello.py
+++ pacote_exemplo/scripts/hello.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
-from dev_aberto import hello
+from datetime import datetime, date
+import dev_aberto
+import gettext
+from babel.dates import format_date, format_datetime, format_time
+from babel.numbers import format_number
+
+
+gettext.install('cli', localedir='locale') 
+
 
 if __name__ == '__main__':
-    date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    today = date.today()
+    print(format_date(today, format='full'))
+
+    commit_date, commit_name = dev_aberto.hello()
+    date_str  = datetime.strptime(commit_date, "%Y-%m-%dT%H:%M:%SZ")
+    print(_('Last commit made in:'), format_datetime(date_str), _('by'), commit_name)
+
+    name = input(_('Input your name: '))
+    print(_('Hello {}'.format(name)))
+
+
